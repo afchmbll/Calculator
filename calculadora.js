11edb6e6e7891e4f436f87bb94fc3ab68ce981b3
afchmbll <93412730+afchmbll@users.noreply.github.com>
2022-02-16 23:59:18 +0000

    Add files via upload

---
+++ calculadora.js
@@ -0,0 +1,49 @@
+let x =[]
+let empty = true;
+
+
+function change(){ /** a funcionar */
+ document.getElementById("ecra").innerHTML=x.join("");
+ if (empty===true){
+    document.getElementById("ecra").innerHTML="Type Something...";
+ }
+
+ let maxWidth = document.getElementById("ecra").clientWidth
+ let width = document.getElementById("ecra").scrollWidth
+ let propEcra = document.getElementById("ecra")
+ let font = window.getComputedStyle(propEcra).getPropertyValue("font-size")
+ if (width>maxWidth){
+     let final = (font.slice(0,4) - 7) + "px"
+     document.getElementById("ecra").style.fontSize  = final;
+ }
+ 
+}
+
+function type(y){
+    empty=false;
+    let value = document.getElementById(y).innerHTML
+    if (y==="teclaEXP"){
+        value="^"
+    }
+    x.push(value)
+    change()
+}
+
+function wipe(){  
+    x.length=0;
+    empty=true;
+    change()
+    document.getElementById("ecra").style.fontSize = "2.1em"
+}
+
+function total(){
+    for (let i in x){
+        if (x[i] === "^"){
+            x[i]="**"
+        }
+    }
+    let str = x.join("")
+    let y = eval(str);
+    x = [y]
+    change()
+}
